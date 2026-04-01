94f12e9003ec85a6ac34163c56f046b721a8ba0b
NabeelLarik <nabeellarik@gmail.com>
2026-04-01 10:38:01 +0500

    again send

--- logenix_qoute_generator.py
+++ logenix_qoute_generator.py
@@ -24,7 +24,7 @@ ONEDRIVE_USER_EMAIL = os.getenv("ONEDRIVE_USER_EMAIL", "nahmed@logenix.pk").stri
 
 ONEDRIVE_PRICES_PATH = os.getenv(
     "ONEDRIVE_PRICES_PATH",
-    "Automation Documents/Logenix/prices_updated.xlsx"
+    "Automation Documents/Logenix/prices.xlsx"
 ).strip()
 
 ONEDRIVE_QUERIES_PATH = os.getenv(
